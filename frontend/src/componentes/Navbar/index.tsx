d7a069feca8d20fd2c47edfeaa4d7df5cf475bdb
Thalita Gonçalves <Thalita.freitas1415@hotmail.com>
2022-01-10 17:50:06 -0300

    Navbar

---
+++ frontend/src/componentes/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GithubIcon } from 'assets/img/Github.svg'
+import './style.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/Thalita1415">
+                        <div className="dsmovie-contact-contein">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+
+}
+
+export default Navbar;
