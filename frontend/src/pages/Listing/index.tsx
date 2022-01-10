edc11072a4057923aff8916f817aa76f88b254f3
Thalita Gonçalves <Thalita.freitas1415@hotmail.com>
2022-01-10 20:18:28 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,34 @@
-import Pagination from "../../componentes/Pagination";
 
-function Listing()
-{
+import Pagination from "../../componentes/Pagination";
+import MovieStars from "../../componentes/MovieScore";
+import MovieScore from "../../componentes/MovieScore";
+import MovieCard from "../../componentes/MovieCard";
+
+function Listing() {
 
     return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
 
-   <Pagination/>
+        </>
+
+
     );
 }
 
