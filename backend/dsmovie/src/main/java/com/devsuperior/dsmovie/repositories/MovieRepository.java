ae27cd2058f978c071631c60bec6673fb2c7687f
Thalita Gonçalves <Thalita.freitas1415@hotmail.com>
2022-01-13 23:25:29 -0300

    Save score

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmovie.repositories;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmovie.entities.Movie;
